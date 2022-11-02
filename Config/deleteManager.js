9f5a89663d78d5dd2dd47ca0db2088bec1b1393f
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-11-03 00:34:10 +0600

    submit on heroku

--- Config/deleteManager.js
+++ Config/deleteManager.js
@@ -10,11 +10,11 @@ const deleteProfileImg = async (req, res, next) => {
 		if (req.currentUser.cover_img === "default-cover.png") {
 			next();
 		} else {
-			const getFilePath = path.resolve(
-				`../client/public/uploads/profile-img/${req.currentUser.cover_img}`
-			);
+			// const getFilePath = path.resolve(
+			// 	`../client/public/uploads/profile-img/${req.currentUser.cover_img}`
+			// );
 
-			// const getFilePath = `./build/uploads/profile-img/${req.currentUser.cover_img}`;
+			const getFilePath = `./build/uploads/profile-img/${req.currentUser.cover_img}`;
 
 			await file.unlink(getFilePath, (error) => {
 				if (error) {
@@ -28,11 +28,11 @@ const deleteProfileImg = async (req, res, next) => {
 		if (req.currentUser.profile_img === "default-profile.png") {
 			next();
 		} else {
-			const getFilePath = path.resolve(
-				`../client/public/uploads/profile-img/${req.currentUser.profile_img}`
-			);
+			// const getFilePath = path.resolve(
+			// 	`../client/public/uploads/profile-img/${req.currentUser.profile_img}`
+			// );
 
-			// const getFilePath = `./build/uploads/profile-img/${req.currentUser.profile_img}`;
+			const getFilePath = `./build/uploads/profile-img/${req.currentUser.profile_img}`;
 
 			await file.unlink(getFilePath, (error) => {
 				if (error) {
@@ -49,11 +49,11 @@ const deleteProfileImg = async (req, res, next) => {
 
 // for deleting user's features
 const deleteFeatureImg = async (req, res, next) => {
-	const getFilePath = path.resolve(
-		`../client/public/uploads/profile-img/${req.query.img}`
-	);
+	// const getFilePath = path.resolve(
+	// 	`../client/public/uploads/profile-img/${req.query.img}`
+	// );
 
-	// const getFilePath = `./build/uploads/profile-img/${req.query.img}`;
+	const getFilePath = `./build/uploads/profile-img/${req.query.img}`;
 
 	await file.unlink(getFilePath, (error) => {
 		if (error) {
